020101c9ffb48e66c2377c56d4d992eed8412975
manriqueoscar087-create <manriqueoscar087@gmail.com>
2026-02-06 15:08:30 -0500

    Add files via upload

---
+++ corte1/c/practica1/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h> // Incluye la biblioteca estándar de entrada/salida
+
+int main() {
+    // printf imprime el texto en la consola
+    printf("Hola, Mundo!\n");                
+    
+    return 0; // Indica que el programa finalizó correctamente
+}
+
